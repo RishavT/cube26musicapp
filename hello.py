852171d695d390feb93c5cfb87c6456b6edceec3
Rishav Thakker <thakker.rishav@gmail.com>
2015-10-21 19:41:51 +0530

    changed hash length

--- hello.py
+++ hello.py
@@ -13,7 +13,7 @@ class User(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
 	username = db.Column(db.String(80), unique=True)
 	email = db.Column(db.String(120), unique=True)
-	pwdhash = db.Column(db.String(54))
+	pwdhash = db.Column(db.String(66))
 	
 	def __init__(self,username,email,password):
 		self.username = username
