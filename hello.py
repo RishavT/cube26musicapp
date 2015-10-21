892941b99bae054a97962e6d61c63e400cfa3821
Rishav Thakker <thakker.rishav@gmail.com>
2015-10-21 19:25:59 +0530

    added pwd

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -12,10 +13,16 @@ class User(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
 	username = db.Column(db.String(80), unique=True)
 	email = db.Column(db.String(120), unique=True)
+	pwdhash = db.Column(db.String(54))
 	
-	def __init__(self,username,email):
+	def __init__(self,username,email,password):
 		self.username = username
-		self.email = email
+		self.email = email.lower()
+		self.set_password(password)
+	def set_password(self,password):
+		self.pwdhash = generate_password_hash(password)
+	def check_password(self,password):
+		return check_password_hash(self.pwdhash, password)
 	def __repr__(self):
 		return '<User %r' % self.username
 
