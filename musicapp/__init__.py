4888584a82b9f1a8276e6009030a95f778318ac5
Rishav Thakker <thakker.rishav@gmail.com>
2015-10-21 21:49:31 +0530

    bugfix

--- musicapp/__init__.py
+++ musicapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
+import os
 
 #from models import *
 #from views import *
@@ -9,3 +10,4 @@ app = Flask(__name__)
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+db = []
