cf889c2fbcc254548d98a2058dbb7ae1fc80300c
Rishav Thakker <thakker.rishav@gmail.com>
2015-10-21 22:21:33 +0530

    bugfix

--- musicapp/__init__.py
+++ musicapp/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
-db = []
+#db = []
 
 from musicapp.routes import *
 from musicapp.models import *
